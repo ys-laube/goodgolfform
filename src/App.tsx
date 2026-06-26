ee6f1b7caee3bfd6faf31f8fa7bf6ad61cc5aab3
aurz <aurz@aurzui-MacBookAir.local>
2026-06-26 16:50:20 +0900

    fix: reconcile scorecard integration duplicates

--- src/App.tsx
+++ src/App.tsx
@@ -470,6 +470,8 @@ export function App() {
     setPlayerHandicapDrafts({ 'player-1': '', 'player-2': '', 'player-3': '', 'player-4': '' });
     setGameUnitDrafts({});
     setScoreDrafts({});
+    setMissionPlayerId('player-1');
+    setMissionCleared(true);
     setShareReady(false);
   }
 
